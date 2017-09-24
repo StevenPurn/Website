503d7670398f5adcf8fac7523378844011a9b650
oops-accidental <smp5119@gmail.com>
2017-09-24 15:34:05 -0700

    hacky fix for form submission

--- action_page.php
+++ action_page.php
@@ -5,27 +5,23 @@ if(isset($_POST['email'])) {
     // CHANGE THE TWO LINES BELOW
     $email_to = "steven.purn@gmail.com";
      
-    $email_subject = "Someone wrote to your from your website!";
-     
-    function died($error) {
-        // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
+    function died() {
+		echo '<script language="javascript">';
+		echo 'alert("Sorry, something went wrong.")';
+		echo '</script>';
+		echo "<meta http-equiv='refresh' content='0;url=http://stevenpurn.com/about.html'>";
+		die();
     }
      
     // validation expected data exists
     if(!isset($_POST['name']) ||
         !isset($_POST['body']) ||
-        !isset($_POST['email']))
-		{
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+        !isset($_POST['email'])){
+        died();       
     }
      
     $name = $_POST['name'];
-    $subject = $_POST['subject'];
+    $email_subject = $_POST['subject'];
     $email_from = $_POST['email'];
     $body = $_POST['body'];
 
@@ -59,13 +55,13 @@ if(isset($_POST['email'])) {
 $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
+mail($email_to, $email_subject, $email_message, $headers);  
 ?>
  
 <!-- place your own success html below -->
- 
-Thanks! I'll get back to you soon!
- 
+echo '<script language="javascript">alert("Thanks! I'll get back to you soon.");</script>';
+echo '<META HTTP-EQUIV=REFRESH CONTENT="0; 'about.html'">';
+
 <?php
 }
 die();
